5e55ad766080a11d5b9526406273014310ab478a
vinay_jadaun <vinay11112003@gmail.com>
2024-08-22 06:10:07 +0530

    new id lester

--- src/pages/room/index.jsx
+++ src/pages/room/index.jsx
@@ -12,8 +12,8 @@ function Roompage() {
     const{roomId}=useParams();
     const vin=window.location.href;
     const myMeeting = async(element)=>{
-        const appID=367181072;
-        const serverSecret="406a296bf949c8e3db07ea973038ee70";
+        const appID=1445511239;
+        const serverSecret="1daedad0e075fcadcc769e57edd36959";
         const kitToken=ZegoUIKitPrebuilt.generateKitTokenForTest(appID,serverSecret,roomId,Date.now().toString(),"Vinay Jadaun");
         const zc=ZegoUIKitPrebuilt.create(kitToken);
         // ${roomId}
